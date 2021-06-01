63809e6582505b9ab24e2346f66cb98e4c2bf73e
Bhuvan Gupta <57099748+bhuvangupta9@users.noreply.github.com>
2021-06-01 19:17:05 +0530

    Update Fireb.js

--- public/js/Fireb.js
+++ public/js/Fireb.js
@@ -1,11 +1,5 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAsJH-eJnCiWPaDl_a9yGbZNPRce0NDE0c",
-    authDomain: "shree-ram-enterprise.firebaseapp.com",
-    projectId: "shree-ram-enterprise",
-    storageBucket: "shree-ram-enterprise.appspot.com",
-    messagingSenderId: "1005653195214",
-    appId: "1:1005653195214:web:f1fe4334e700c9725e139f",
-    measurementId: "G-9BYF3ZV4V3"
+    //firebase config
 };
 
 // Initialize Firebase
